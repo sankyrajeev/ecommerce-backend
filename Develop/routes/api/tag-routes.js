daf665e366667b06a29d1de80eb57d477fec92aa
Vontell <sankarsh.rajeev@gmail.com>
2022-11-12 13:17:22 -0800

    category route added

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -28,10 +28,8 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Product data
   try {
 
-    const tagDataId = await Tag.findone({
-      where: {
-        id: req.params.id
-      },
+    const tagDataId = await Tag.findByPk(req.params.id,{
+     
       include: [
         {
           model: Product,
@@ -43,7 +41,7 @@ router.get('/:id', async (req, res) => {
       res.status(404).json({ message: "TAg with that ID Does not exist" });
     }
 
-    ews.status(200).json(tagDataId);
+    res.status(200).json(tagDataId);
 
   }
   catch (err) {
@@ -65,15 +63,15 @@ router.post('/', (req, res) => {
 });
 
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
-  Tag.update(req.body)
-    .then((tag) => {
-      res.status(200).json(tag)
-    })
-    .catch((err) => {
-      res.status(400).json(err)
-    })
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((tag) => res.status(200).json(tag))
+    .catch((err) => res.status(404).json(err));
 });
+
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
